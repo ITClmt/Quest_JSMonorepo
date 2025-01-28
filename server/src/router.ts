5ec4e1144355cd4af98496dd887aa065bafbae72
Clément.A <125081819+ITClmt@users.noreply.github.com>
2025-01-28 16:29:23 +0100

    04 - JS Monorepo : Routing avancé Done

--- server/src/router.ts
+++ server/src/router.ts
@@ -13,16 +13,26 @@ router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+// Define program-related routes
+import programActions from "./modules/program/programActions";
+
+router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
 /* ************************************************************************* */
 
 // Declaration of a "Welcome" route
 
 import sayActions from "./modules/say/sayActions";
-import programActions from "./modules/program/programActions";
 
 router.get("/", sayActions.sayWelcome);
-router.get("/api/programs", programActions.browse);
 
 /* ************************************************************************* */
+
+// Define category-related routes
+import categoryActions from "./modules/category/categoryActions";
+
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 
 export default router;
